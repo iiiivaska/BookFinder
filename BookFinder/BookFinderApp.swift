5adc3fb32650d7008dfda965e2f24485c609bd14
iiiivaska <iiiivaska@yandex.ru>
2022-05-17 15:35:42 +0300

    Initial Commit

---
+++ BookFinder/BookFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookFinderApp.swift
+//  BookFinder
+//
+//  Created by Василий Буланов on 17.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct BookFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
